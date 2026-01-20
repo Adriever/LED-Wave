45cdaac3670cf7d1eb52ac5a464ffe4b1ef9ef84
Adriever <k0911351@student.kirkwood.edu>
2026-01-20 09:46:21 -0600

    Add files via upload

--- Project2_LED_Wave.ino
+++ Project2_LED_Wave.ino
@@ -1,3 +1,4 @@
+int d = 100;
 void setup()
 {
 pinMode(2, OUTPUT);
@@ -6,31 +7,14 @@ pinMode(4, OUTPUT);
 pinMode(5, OUTPUT);
 pinMode(6, OUTPUT);
 }
-
 void loop()
 {
-digitalWrite(2, HIGH);
-delay(100);
-digitalWrite(2, LOW);
-digitalWrite(3, HIGH);
-delay(100);
-digitalWrite(3, LOW);
-digitalWrite(4, HIGH);
-delay(100);
-digitalWrite(4, LOW);
-digitalWrite(5, HIGH);
-delay(100);
-digitalWrite(5, LOW);
-digitalWrite(6, HIGH);
-delay(100);
-digitalWrite(6, LOW);
-digitalWrite(5, HIGH);
-delay(100);
-digitalWrite(5, LOW);
-digitalWrite(4, HIGH);
-delay(100);
-digitalWrite(4, LOW);
-digitalWrite(3, HIGH);
-delay(100);
-digitalWrite(3, LOW);
+
+for ( int a = 2; a < 7 ; a++ )
+{
+digitalWrite(a, HIGH);
+delay(d);
+digitalWrite(a, LOW);
+delay(d);
+}
 }
